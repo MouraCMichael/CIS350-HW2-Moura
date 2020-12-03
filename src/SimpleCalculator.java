805b74b0628afba1b1b8dd3d18153f4b4243776d
Corey Moura <Corey@Coreys-MBP.attlocal.net>
2020-12-03 14:17:56 -0500

    Test of Travis.ic

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,32 @@
+package cmm_cis350;
+
+public class SimpleCalculator {
+	
+
+	public static final void main(String[] args) {
+		System.out.println("Hey there CIS350.");
+		
+		//System.out.println(div(10,2));		// public static int to verify
+
+	}
+
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
+	
+	
+	
+}
